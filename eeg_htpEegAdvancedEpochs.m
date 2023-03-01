1520007209ffbb339e0943865fcbbfdcdbff5a5b
Ernie Pedapati <ernest.pedapati@cchmc.org>
2023-03-01 18:19:59 -0500

    Update eeg_htpEegAdvancedEpochs.m

--- eeg_htpEegAdvancedEpochs.m
+++ eeg_htpEegAdvancedEpochs.m
@@ -1,4 +1,4 @@
-function [epochEEG, eventtbl_epoch] = eeg_htpEegAdvancedEpochs(EEG, main_trigger, backup_trigger, epoch_length, varargin)
+function [outEEG, eventtbl_epoch] = eeg_htpEegAdvancedEpochs(EEG, main_trigger, backup_trigger, epoch_length, varargin)
 % Advanced epoching example for complex ERP paradigms
 % Inputs:
 % - EEG: input EEG SET file
@@ -21,8 +21,6 @@ addRequired(p, 'epoch_length', @isnumeric);
 addParameter(p, 'TimeUnit', 1e-3, @isnumeric);
 addParameter(p, 'SaveCSV', false, @islogical);
 addParameter(p, 'outputDir', pwd, @isfolder);
-addParameter(p, 'withCondition', false, @mustBeNumericOrLogical);
-
 parse(p, EEG, main_trigger, backup_trigger, epoch_length, varargin{:});
 
 % Convert events to user-friendly table
@@ -32,7 +30,6 @@ eventtbl_raw = array2table(eventout, 'VariableNames', fields);
 % Recreate new event table only using relevant columns
 eventtbl = table(eventtbl_raw.trial_type, eventtbl_raw.Condition, eventtbl_raw.latency, 'VariableNames', {'type','condition', 'latency'});
 eventtbl_select = eventtbl(ismember(eventtbl.type, [main_trigger, backup_trigger]),:);
-eventtbl_select.LaggedCondition = [eventtbl_select.condition(2:end); NaN];
 
 % Get counts
 stimcounts = cell2table(tabulate(eventtbl_select{:,'type'}), 'VariableNames', {'type','count','percentage'});
@@ -40,12 +37,15 @@ findCount = @(T, type) sum(table2array(T(ismember(T.type, type), 'count')));
 
 % Error check if photosensor and stimulus counts match
 % TODO: Add action logger for logging invalid photosensor data
+%       Will "lag" condition to align with photosensor if sensor is valid
 if(isequal(findCount(stimcounts, main_trigger), findCount(stimcounts, backup_trigger)))
     disp('Photo sensor data is valid.')
     valid_event = main_trigger;
+    eventtbl_select.LaggedCondition = [eventtbl_select.condition(2:end); NaN];
 else
     disp('# of DINs does not equal # of stimuli.');
     valid_event = backup_trigger;
+    eventtbl_select.LaggedCondition = [eventtbl_select.condition(1:end)];
 end
 inevents =  eventtbl_select(ismember(eventtbl_select.type, valid_event),:);
 
@@ -59,13 +59,15 @@ epochEEG = pop_epoch(eventEEG, valid_event, epoch_length, 'epochinfo', 'yes');
 epochEEG2 = pop_selectevent(epochEEG, 'latency','-.1 <= .1','deleteevents','on');
 
 % Query raw event table
-[eventout_epoch, fields_epoch] = eeg_eventformat(epochEEG2.epoch, 'array',  {'type','latency'});
+[eventout_epoch, fields_epoch] = eeg_eventformat(epochEEG2.epoch, 'array',  {'type','latency','condition'});
 
 % error checking for empty epochs
 assert(~isempty(eventout_epoch), 'No epochs available - check event codes or timeunit.')
 
 % Verify event conversion with table output
 eventtbl_epoch = array2table(eventout_epoch, 'VariableNames', fields_epoch);
+
+outEEG = epochEEG2;
 
 % Save output as a CSV file
 if p.Results.SaveCSV
